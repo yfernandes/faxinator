c585ef749589058814fbde4714ec4b2bf34e34a8
yfernandes <yagoalmeida@gmail.com>
2020-08-14 19:50:23 -0300

    Table component draft

---
+++ packages/web/src/components/Table/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+interface Props {}
+
+const Table = (props: Props) => {
+	return <Container>Test</Container>;
+};
+
+export default Table;
